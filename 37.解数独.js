52381c269e92ea44ce4d550aa2478a69652f2c07
FannyZhou <2644339003@qq.com>
2020-03-16 16:31:03 +0800

    37.解数独-答案
    Signed-off-by: FannyZhou <2644339003@qq.com>

--- 37.解数独.js
+++ 37.解数独.js
@@ -15,7 +15,9 @@ l = () => {} ;
 
 /* 
 思路:  递归 + 回溯
-从第一个格子开始遍历每一个空格, 计算该空格可填入数字, 遇到无法填入的空格则撤销操作(回溯), 尝试填入其他数字, 没有其他数字可填就再往回撤销,
+从第一个格子开始遍历每一个空格, 计算该空格可填入数字,
+遇到无法填入的空格则撤销操作(回溯), 尝试填入其他数字,
+没有其他数字可填就再往回撤销,
 以此类推, 直到最后一个空格, 表示数独完成
 
 求出数字填充范围:
@@ -24,7 +26,7 @@ l = () => {} ;
 3. 块 可填充数
 > 求交集
 */
-var solveSudoku = function(board) {
+var solveSudoku1 = function(board) {
   function findAvailableNums(board, i, j) { // 还可再优化
     let res = ['', 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for(let k = 0; k < 9; k++) {
@@ -73,5 +75,49 @@ var solveSudoku = function(board) {
   }
   solveBoard(board, 0, 0);
 };
+/* 思路：
+  1. 遍历每一格, 为空则 尝试填入 1 - 9
+  2. 失败就回溯
+  3. 循环到最后成功则 结束
+  */
+
+var solveSudoku = function(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] !== '.') continue;
+      for (let k = 1; k <= 9; k++) {
+        if (!isValid(i, j, k.toString())) { continue; }
+        board[i][j] = k.toString(); // 放入数字
+        if (solveSudoku(board)) { return true; }
+        board[i][j] = '.'; // 回溯
+      }
+      return false; // 没找到可放的数字, 失败
+    }
+  }
+  return true;
+
+  function isValid(row, col, k) {
+    // 验证 行 + 列
+    for (let i = 0; i < 9; i++) {
+      if (board[row][i] === k || board[i][col] === k) {
+        return false;
+      }
+    }
+
+    // 验证 3 X 3 方块 ■ 先求左上角的点
+    const x = Math.floor(row / 3) * 3;
+    const y = Math.floor(col / 3) * 3;
+    for (let i = x; i < x + 3; i++) {
+      for (let j = y; j < y + 3; j++) {
+        if (board[i][j] === k) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
+
 // @lc code=end
 
